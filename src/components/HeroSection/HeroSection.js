af1ee563b3a49c73a030cb322e6deda02803f779
ssemugenyi <ssemugenyiisaac2@gmail.com>
2023-11-27 01:07:39 +0300

    new: added hero section without a slider

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styles from "./HeroSection.module.css"
+
+const HeroSection = () => {
+    return (
+        <div className={styles.hero_section}>
+            <div className={styles.content}>
+                <h2>TRAVEL TO EXPLORE</h2>
+                <p>
+                    Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed
+                    Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna
+                    Aliqua. Ultricies Mi Eget
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
